a80b1ffe82c02f0dc13ddfcb382e5ef25057902d
roceys <roceys@users.noreply.github.com>
2018-06-30 04:52:24 +0800

    fixed.

--- src/main/java/com/roceys/org/db/Export2Sqlite.java
+++ src/main/java/com/roceys/org/db/Export2Sqlite.java
@@ -10,7 +10,60 @@ public class Export2Sqlite {
     private static SQLiteDataSource sqLiteDataSource;
     private static QueryRunner queryRunner;
     private static final String SQLITE_DB = "jdbc:sqlite:D:\\SQLiteStudio\\eleme";
-    private static final String SQL_ELEME_ORDER = "";
+    private static final String SQL_ELEME_ORDER = "INSERT INTO Eorder (\n" +
+            "                       image_hash,\n" +
+            "                       name,\n" +
+            "                       price,\n" +
+            "                       quantity,\n" +
+            "                       marks,\n" +
+            "                       unique_id,\n" +
+            "                       restaurant_name,\n" +
+            "                       restaurant_image_hash,\n" +
+            "                       restaurant_id,\n" +
+            "                       restaurant_type,\n" +
+            "                       restaurant_valid,\n" +
+            "                       total_amount,\n" +
+            "                       total_quantity,\n" +
+            "                       rated_point,\n" +
+            "                       operation_rate,\n" +
+            "                       is_brand,\n" +
+            "                       is_deletable,\n" +
+            "                       is_from_openapi,\n" +
+            "                       is_new_pay,\n" +
+            "                       id,\n" +
+            "                       courier_phone,\n" +
+            "                       created_timestamp,\n" +
+            "                       packing_quantity,\n" +
+            "                       packing_total_price,\n" +
+            "                       deliver_price\n" +
+            "                   )\n" +
+            "                   VALUES (\n" +
+            "                       'image_hash',\n" +
+            "                       'name',\n" +
+            "                       'price',\n" +
+            "                       'quantity',\n" +
+            "                       'marks',\n" +
+            "                       'unique_id',\n" +
+            "                       'restaurant_name',\n" +
+            "                       'restaurant_image_hash',\n" +
+            "                       'restaurant_id',\n" +
+            "                       'restaurant_type',\n" +
+            "                       'restaurant_valid',\n" +
+            "                       'total_amount',\n" +
+            "                       'total_quantity',\n" +
+            "                       'rated_point',\n" +
+            "                       'operation_rate',\n" +
+            "                       'is_brand',\n" +
+            "                       'is_deletable',\n" +
+            "                       'is_from_openapi',\n" +
+            "                       'is_new_pay',\n" +
+            "                       'id',\n" +
+            "                       'courier_phone',\n" +
+            "                       'created_timestamp',\n" +
+            "                       'packing_quantity',\n" +
+            "                       'packing_total_price',\n" +
+            "                       'deliver_price'\n" +
+            "                   )";
 
     public int insert(Orders orders) throws Exception {
         sqLiteDataSource = new SQLiteDataSource();
